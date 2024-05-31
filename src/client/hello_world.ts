1db9218fae16ab71ed750de7d5d8eaf3f6d74a82
Lean <leandrop@eliteautomationgroup.com>
2024-05-31 15:57:16 -0500

    project

--- src/client/hello_world.ts
+++ src/client/hello_world.ts
@@ -14,6 +14,8 @@ import {
 import fs from 'mz/fs';
 import path from 'path';
 import * as borsh from 'borsh';
+import * as BufferLayout from "@solana/buffer-layout";
+import {Buffer} from 'buffer';
 
 import {getPayer, getRpcUrl, createKeypairFromFile} from './utils';
 
@@ -195,6 +197,30 @@ export async function checkProgram(): Promise<void> {
   }
 }
 
+function createImplementInstruction(): Buffer {
+  const layout = BufferLayout.struct([BufferLayout.u8('instruction')]);
+  const data = Buffer.alloc(layout.span);
+  layout.encode({instruction: 0}, data);
+  return data;
+}
+
+function createDecrementInstruction(): Buffer {
+  const layout = BufferLayout.struct([BufferLayout.u8('instruction')]);
+  const data = Buffer.alloc(layout.span);
+  layout.encode({instruction: 1}, data);
+  return data;
+}
+
+function createSetInstruction(): Buffer {
+  const layout = BufferLayout.struct([
+    BufferLayout.u8('instruction'),
+    BufferLayout.u32('value')
+  ]);
+  const data = Buffer.alloc(layout.span);
+  layout.encode({instruction: 2, value: 100}, data);
+  return data;
+}
+
 /**
  * Say hello
  */
@@ -203,7 +229,7 @@ export async function sayHello(): Promise<void> {
   const instruction = new TransactionInstruction({
     keys: [{pubkey: greetedPubkey, isSigner: false, isWritable: true}],
     programId,
-    data: Buffer.alloc(0), // All instructions are hellos
+    data: createImplementInstruction(), // All instructions are hellos
   });
   await sendAndConfirmTransaction(
     connection,
@@ -211,6 +237,8 @@ export async function sayHello(): Promise<void> {
     [payer],
   );
 }
+
+
 
 /**
  * Report the number of times the greeted account has been said hello to
